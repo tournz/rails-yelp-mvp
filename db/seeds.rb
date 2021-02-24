1f08ad1d1245c779d2f72d8ca2ad98996dd844ce
Zacharie Tournant ztournant@icloud.com <ztournant@icloud.com>
2021-02-24 15:37:10 +0300

    Architecture done without embedding the reviews form and without format

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+
+20.times do
+  restaurant = Restaurant.create(name: Faker::Restaurant.name,
+                                 address: Faker::Address.full_address,
+                                 phone_number: Faker::PhoneNumber.cell_phone,
+                                 category: %w(Chinese Japanese French Italian Belgian).sample)
+  puts "Created #{restaurant.name}"
+end
+puts "#{Restaurant.count} restaurants were added to the database"
